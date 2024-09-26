766c44152cea376ba90e1d927531ae082c6c5bcc
theblazetimes <theblazetimes@gmail.com>
2024-09-26 10:34:06 +0530

    Update mediumish.js

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -1,129 +1,91 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
+    
+    // Cache frequently accessed elements
+    const $alertbar = $('.alertbar');
+    const $navbar = $('nav');
+    const $siteContent = $('.site-content');
+    const navbarHeight = $navbar.outerHeight();
+    
+    // Fix for IE/Edge object cover issue
+    if ('objectFit' in document.documentElement.style === false) {
+        $('.featured-box-img-cover').each(function() {
+            const $img = $(this);
+            const imgSrc = 'url(' + $img.attr('src') + ')';
+            const $imgParent = $img.parent();
+            const $div = $('<div></div>');
 
-    //fix for stupid ie object cover
-    if (document.documentMode || /Edge/.test(navigator.userAgent)) {
-      jQuery('.featured-box-img-cover').each(function(){
-          var t = jQuery(this),
-              s = 'url(' + t.attr('src') + ')',
-              p = t.parent(),
-              d = jQuery('<div></div>');
-  
-          p.append(d);
-          d.css({
-              'height'                : '290',
-              'background-size'       : 'cover',
-              'background-repeat'     : 'no-repeat',
-              'background-position'   : '50% 20%',
-              'background-image'      : s
-          });
-          t.hide();
-      });
+            $imgParent.append($div);
+            $div.css({
+                'height': '290px',
+                'background-size': 'cover',
+                'background-repeat': 'no-repeat',
+                'background-position': '50% 20%',
+                'background-image': imgSrc
+            });
+            $img.hide();
+        });
     }
 
-    // alertbar later
-    $(document).scroll(function () {
-        var y = $(this).scrollTop();
-        if (y > 280) {
-            $('.alertbar').fadeIn();
-        } else {
-            $('.alertbar').fadeOut();
-        }
+    // Alertbar visibility on scroll
+    $(document).scroll(function() {
+        const scrollTop = $(this).scrollTop();
+        scrollTop > 280 ? $alertbar.fadeIn() : $alertbar.fadeOut();
     });
 
-
-    // Smooth on external page
-    $(function() {
-      setTimeout(function() {
-        if (location.hash) {
-          /* we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 */
-          window.scrollTo(0, 0);
-          target = location.hash.split('#');
-          smoothScrollTo($('#'+target[1]));
-        }
-      }, 1);
-
-      // taken from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
-      $('a[href*=\\#]:not([href=\\#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          smoothScrollTo($(this.hash));
-          return false;
-        }
-      });
-
-      function smoothScrollTo(target) {
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-        }
-      }
-    });
-    
-    
-    // Hide Header on on scroll down
-    var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    var navbarHeight = $('nav').outerHeight();
-
-    $(window).scroll(function(event){
-        didScroll = true;
-    });
-
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 250);
-
-    function hasScrolled() {
-        var st = $(this).scrollTop();
-        
-        // Make sure they scroll more than delta
-        if(Math.abs(lastScrollTop - st) <= delta)
-            return;
-
-        // If they scrolled down and are past the navbar, add class .nav-up.
-        // This is necessary so you never see what is "behind" the navbar.
-        if (st > lastScrollTop && st > navbarHeight){
-            // Scroll Down            
-            $('nav').removeClass('nav-down').addClass('nav-up'); 
-            $('.nav-up').css('top', - $('nav').outerHeight() + 'px');
-           
-        } else {
-            // Scroll Up
-            if(st + $(window).height() < $(document).height()) {               
-                $('nav').removeClass('nav-up').addClass('nav-down');
-                $('.nav-up, .nav-down').css('top', '0px');             
+    // Smooth scroll for anchor links
+    $('a[href*="#"]:not([href="#"])').click(function(event) {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            const target = $(this.hash);
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                event.preventDefault();
             }
         }
+    });
 
-        lastScrollTop = st;
-    }
-        
-    $('.site-content').css('margin-top', $('header').outerHeight() + 'px');  
-    
-    // spoilers
-     $(document).on('click', '.spoiler', function() {
+    // Hide header on scroll down
+    let lastScrollTop = 0;
+    const delta = 5;
+
+    $(window).scroll(function() {
+        const st = $(this).scrollTop();
+        if (Math.abs(lastScrollTop - st) > delta) {
+            if (st > lastScrollTop && st > navbarHeight) {
+                // Scroll down
+                $navbar.removeClass('nav-down').addClass('nav-up').css('top', -navbarHeight + 'px');
+            } else if (st + $(window).height() < $(document).height()) {
+                // Scroll up
+                $navbar.removeClass('nav-up').addClass('nav-down').css('top', '0px');
+            }
+            lastScrollTop = st;
+        }
+    });
+
+    // Adjust site content margin based on header height
+    $siteContent.css('margin-top', $('header').outerHeight() + 'px');
+
+    // Spoilers click handler
+    $(document).on('click', '.spoiler', function() {
         $(this).removeClass('spoiler');
-     });
-    
- });   
-
-// deferred style loading
-var loadDeferredStyles = function () {
-	var addStylesNode = document.getElementById("deferred-styles");
-	var replacement = document.createElement("div");
-	replacement.innerHTML = addStylesNode.textContent;
-	document.body.appendChild(replacement);
-	addStylesNode.parentElement.removeChild(addStylesNode);
-};
-var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-	window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-if (raf) raf(function () {
-	window.setTimeout(loadDeferredStyles, 0);
+    });
 });
-else window.addEventListener('load', loadDeferredStyles);
+
+// Deferred style loading
+const loadDeferredStyles = () => {
+    const addStylesNode = document.getElementById('deferred-styles');
+    if (addStylesNode) {
+        const replacement = document.createElement('div');
+        replacement.innerHTML = addStylesNode.textContent;
+        document.body.appendChild(replacement);
+        addStylesNode.parentElement.removeChild(addStylesNode);
+    }
+};
+
+const raf = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+if (raf) {
+    raf(() => setTimeout(loadDeferredStyles, 0));
+} else {
+    window.addEventListener('load', loadDeferredStyles);
+}
